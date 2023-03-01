1d94ce7475f637a9c3f3b90152c403cecc660a86
TianyuWuHenry <66983833+TianyuWu-Henry@users.noreply.github.com>
2023-03-01 16:36:58 +0800

    Update DistFit.py

--- Simulation/Distribution/DistFit.py
+++ Simulation/Distribution/DistFit.py
@@ -1,64 +1,115 @@
-import numpy as np
 import pandas as pd
-from sklearn.mixture import GaussianMixture
+import numpy as np
 import random
+from google.colab import drive
+drive.mount('/content/drive')
 
-# A class to fit distribution to Ethereum transaction attributes, which are Gas Limit, Used Gas, Gas Price as well as CPU Time
+class User:
+    def __init__(self, gas_price):
+        self.gas_price = gas_price
 
-class DistFit():
+class Miner:
+    def __init__(self, alpha, target):
+        self.alpha = alpha
+        self.target = target
+        self.is_dark = target < alpha
 
-    txGas = None
-    txGasPrice = None
-    utlization = None
-    x = 0
-    
-    df2 = pd.read_csv('/Users/tianyuwu/Downloads/Transaction-Latency-main/Distribution/tx_data.csv')
-    
-    def fit():
+    def choose_pool(self, mempool_fee, dark_fee):
+        if self.is_dark:
+            if dark_fee > mempool_fee:
+                return "dark"
+            else:
+                return "mempool"
+        else:
+            if mempool_fee > dark_fee:
+                return "mempool"
+            else:
+                return "dark"
 
-        txUsedGas2 = np.log(DistFit.df2['receipt_gas_used']).values.reshape(-1,1)
-        txPrice = np.log(DistFit.df2['gas_price']+0.001).values.reshape(-1,1)
-        DistFit.txGas, DistFit.txGasPrice = DistFit.creation_fit(txUsedGas2,txPrice)
+class Arbitrager:
+    def __init__(self, gamma):
+        self.gamma = gamma
 
+    def arbitrage(self, mempool_fee, dark_fee):
+        profit = (mempool_fee - dark_fee) / dark_fee
+        if profit > self.gamma:
+            return True
+        else:
+            return False
 
-    def creation_fit(df,df1):
-        K = 5
-        g = GaussianMixture(n_components = K)
-        gmm = g.fit(df)
-        
-        eps = 0.001
-        K = 65
-        gg = GaussianMixture(n_components = K)
-        ggmm = gg.fit(df1)
+# Set up the simulation parameters
+num_users = 100
+num_miners = 100
+num_arbitragers = 2
+alpha = 0.5
+target = 0.1
+gamma = 0.1
+num_time_steps = 1000
+mempool = []
+dark_pool = []
+mempool_users = []
+dark_users = []
 
-        return gmm, ggmm
+# Create the miners
+miners = [Miner(alpha, target) for i in range(num_miners)]
 
+df = pd.read_csv('/content/tx_data1.csv', index_col = 0)
+gas_price = df['gas_price'][:num_users].tolist()
 
-    def sample_transactions(n):
-        gastx = DistFit.txGas.sample(n)[0]
-        gastx = np.exp(gastx).flatten().round()
-        gastx[gastx < 21000] = 21000
-        gastx[gastx > 30000000] = 30000000
+# Create the users
+users = [User(gas_price[i]) for i in range(num_users)]
 
-        gasprice = DistFit.txGasPrice.sample(n)[0]
-        gasprice = np.exp(gasprice).flatten().round()
+# Create the arbitrager
+arbitragers = [Arbitrager(gamma) for i in range(num_arbitragers)]
 
-        UT = random.uniform(0,100)
+# Initialize the fee variables
+mempool_fee = 0
+dark_fee = 0
 
+# Loop over time steps
+for t in range(num_time_steps):
+    # Set the new fees
+    mempool_fee = random.uniform(0, 100)
+    dark_fee = random.uniform(0, 100)
 
-        if (UT <= 6.57):
-            utlization = 0
-        elif (UT <= 27.07):
-            utlization = random.uniform(0.1,20)
-        elif (UT <= 34.75):
-            utlization = random.uniform(20.1,40)
-        elif (UT <= 40.34):
-            utlization = random.uniform(40.1,60)
-        elif (UT <= 44.97):
-            utlization = random.uniform(60.1,80)
-        elif (UT <= 48.91):
-            utlization = random.uniform(80.1,95)
-        elif (UT <= 100):
-            utlization = random.uniform(95.1,100)     
+    # Loop over the miners
+    for miner in miners:
+        # Choose the pool
+        pool_choice = miner.choose_pool(mempool_fee, dark_fee)
 
-        return gastx, gasprice, utlization
+        # If the miner chooses the mempool, add them to the mempool list
+        if pool_choice == "mempool":
+            mempool.append(miner)
+
+        # If the miner chooses the dark pool, add them to the dark pool list
+        elif pool_choice == "dark":
+            dark_pool.append(miner)
+
+    # Loop over the users
+    for user in users:
+        # Choose the pool with the lowest fee
+        if mempool_fee < dark_fee:
+            user_choice = "mempool"
+        else:
+            user_choice = "dark"
+
+        # Add the user to the chosen pool
+        if user_choice == "mempool":
+            mempool_users.append(user)
+        else:
+            dark_users.append(user)
+
+    # Loop over the arbitragers
+    for arbitrager in arbitragers:
+        # Check if there is a profitable arbitrage opportunity
+        if arbitrager.arbitrage(mempool_fee, dark_fee):
+            # If the mempool fee is higher, buy from the dark pool
+            if mempool_fee > dark_fee:
+                for user in dark_users:
+                    if user.gas_price >= dark_fee:
+                        user.gas_price = mempool_fee
+            # If the dark fee is higher, buy from the mempool
+            else:
+                for user in mempool_users:
+                    if user.gas_price >= mempool_fee:
+                        user.gas_price = dark_fee
